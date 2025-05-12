3345cb4491ec285783bea56dc23bfafc98899f47
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2025-05-12 00:10:56 -0300

    feat/ auto request server

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://control-361-frontend.vercel.app/',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://control-361-frontend.vercel.app',
+        'http://localhost:3000',
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   const config = new DocumentBuilder()
@@ -21,5 +31,11 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
+  const SELF_URL: string = process.env.API_URL ?? 'http://localhost:3000';
+  setInterval(() => {
+    fetch(SELF_URL)
+      .then((res) => console.log(`[AutoPing] Status: ${res.status}`))
+      .catch((err) => console.error('[AutoPing] Erro:', err));
+  }, 40000);
 }
 bootstrap();
