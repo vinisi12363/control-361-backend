239f5d950a66d70e77a9023a96f98cf9dc03a99e
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2025-05-11 23:55:52 -0300

    fix/cors adjustments

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://control-361-frontend.vercel.app/', 
-    credentials: true, 
+    origin: 'https://control-361-frontend.vercel.app/',
+    credentials: true,
   });
   const config = new DocumentBuilder()
     .setTitle('API de Locação de Veículos')
